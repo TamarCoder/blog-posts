ea5078ffeb811375004a0aa39972511c288d7493
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2026-01-05 14:40:36 +0400

    add  variable and fonts

---
+++ app/Components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import  styles from './Footer.module.scss';
+
+export const  Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2024 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
